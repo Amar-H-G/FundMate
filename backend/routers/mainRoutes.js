7c727020fb89eac09e3a576fdcb0bc592f693ac3
Amar Patra <amarpatra932@gmail.com>
2025-06-21 23:29:25 +0530

    Solve error in backend  folder regex error

--- backend/routers/mainRoutes.js
+++ backend/routers/mainRoutes.js
@@ -7,8 +7,4 @@ const router = express.Router();
 router.use("/api/user", authRoutes);
 router.use("/api/funds", fundRoutes);
 
-router.use("*", (req, res) => {
-  res.status(404).json({ message: "Not Found" });
-});
-
 module.exports = router;
